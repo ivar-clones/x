197632cc6382ad5f93f7f17b0ffd8f9005a97ee8
Varun Gupta <varungupta2015135@gmail.com>
2024-09-08 19:07:28 +0530

    added user repo code

--- server/pkg/repository/repository.go
+++ server/pkg/repository/repository.go
@@ -1,9 +1,15 @@
 package repository
 
-import "github.com/jackc/pgx/v5/pgxpool"
+import (
+	"context"
+	"log"
+	"x/pkg/model"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
 
 type Repository interface {
-
+	GetAllUsers() ([]model.User, error)
 }
 
 type repository struct {
@@ -14,4 +20,32 @@ func New(db *pgxpool.Pool) Repository {
 	return &repository{
 		db: db,
 	}
+}
+
+func (r *repository) GetAllUsers() ([]model.User, error) {
+	var users []model.User
+	rows, err := r.db.Query(context.Background(), "select * from users")
+	if err != nil {
+		log.Printf("error fetching users: %+v", err)
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	for rows.Next() {
+		var user model.User
+		err = rows.Scan(&user)
+		if err != nil {
+			return nil, err
+		}
+
+		users = append(users, user)
+	}
+
+	if rows.Err() != nil {
+		log.Printf("error scanning rows: %+v", rows.Err())
+		return nil, rows.Err()
+	}
+
+	return users, nil
 }
